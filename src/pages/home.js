cfaf95fb189cf8eba0cbe731e9dccf368e0bc855
rioaldie <rioaldierwanto@gmail.com>
2023-03-10 07:12:28 +0700

    finished v-1 hero

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,5 @@
 import { Box, Container } from '@mui/material';
 import React from 'react';
-import Feature from '../components/Feature';
-import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import ResponsiveAppBar from '../layout/Navbar';
 
