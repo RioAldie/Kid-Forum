7c0df1b52fe0449f10333ca5e7d6b487d34ba437
rioaldie <rioaldierwanto@gmail.com>
2023-03-12 06:36:39 +0700

    add form report

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import { Container } from '@mui/system';
 import React from 'react';
+import FormReport from '../components/FormReport';
 import Services from '../components/Services';
 import Sticker from '../components/Sticker';
 
@@ -10,13 +11,17 @@ const Report = () => {
       sx={{
         display: 'flex',
         alignItems: 'center',
-        flexDirection: 'column',
+        flexDirection: 'row',
         padding: '0',
         width: '100%',
         overflow: 'hidden',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        gap: '50px',
       }}>
-      <Services />
+      <FormReport />
       <Sticker />
+      <Services />
     </Box>
   );
 };
